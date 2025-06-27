1f56b672826f6e80b5b17ca9ca9a54869f3de7d6
rahulkumarparida <rahulkumarparida52@gmail.com>
2025-06-27 12:31:36 -0700

    done

--- init_db.py
+++ init_db.py
@@ -8,6 +8,7 @@ DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
 DB_PORT = os.environ.get('DB_PORT')
+DATABASE_URL = os.environ.get('DATABASE_URL') 
 
 conn = psycopg2.connect(
     dbname=DB_NAME,
