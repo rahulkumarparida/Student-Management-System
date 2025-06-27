9e8110090fdc81a16e8895324f50569742603dab
rahulkumarparida <rahulkumarparida52@gmail.com>
2025-06-27 12:17:01 -0700

    changes in init

--- init_db.py
+++ init_db.py
@@ -2,12 +2,14 @@ import psycopg2
 # app.py
 import os
 
+ 
 
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
 DB_PORT = os.environ.get('DB_PORT')
+DATABASE_URL = os.environ.get('DATABASE_URL') 
 
 conn = psycopg2.connect(
     dbname=DB_NAME,
@@ -22,11 +24,18 @@ print("Connected successfully")
 
 cursor = conn.cursor()
 
+
+
 cursor.execute('''CREATE TABLE IF NOT EXISTS department(id serial PRIMARY KEY,name text UNIQUE NOT NULL , fees int , duration int DEFAULT 4);''')
 print("Table Created")
 cursor.execute('''INSERT INTO  department(name , fees) VALUES ('Computer Science' , 120000) , ('Mechanical' , 110000) , ('Civil', 100000) , ('Electrical' , 115000);''')
 print('Data added sucessfully')
 
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS students (id serial PRIMARY KEY,sname VARCHAR(100),age INT,rollno VARCHAR(50),regn INT,dept_id INT REFERENCES department(id) );''')
+print("Table Created")
+cursor.execute('''INSERT INTO students (id, sname, age, rollno, regn, dept_id) VALUES(7, 'Rony Rathore', 20, 'CS-23-51', 23095, 1),(8, 'Rohit Nanda', 21, 'CS-23-52', 23096, 2),(9, 'Anusha Patra', 19, 'CS-23-53', 23097, 1),(10, 'Amit Kumar', 22, 'CS-23-54', 23098, 3); ''')
+print('Data added sucessfully')
 
 conn.commit()
 conn.close()
