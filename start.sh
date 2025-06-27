2209ee5fe9bfe6c0f80e4d756c654ce7a43f41ba
rahulkumarparida <rahulkumarparida52@gmail.com>
2025-06-27 13:28:01 -0700

    Removed Procfile and start.sh (now using Start Command)

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "🚀 Starting server..."
-echo "🔁 Running init_db.py every time (temporary fix)..."
-python init_db.py
-
-echo "▶️ Starting Flask app using gunicorn..."
-gunicorn app:app --bind=0.0.0.0:$PORT
